5a6e37a2fbfd1c9db6deab87a4fe77c73d5f08d6
MonsterBasket <blazza1@hotmail.com>
2022-07-25 14:24:30 +1000

    I can't get the tests to pass, but it seems to be doing what it should

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Add your code here
+let dummyObject = {
+    Name: "James",
+    Email: "James@blaskett.com"
+  };
+  
+  function submitData(name, email){
+    let user = {"name":name, "email":email}
+    let config = {
+        method: "PORST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(user)
+    };
+
+    return fetch("http://localhost:3000/users", config)
+            .then(response => response.json())
+            .then(object => document.querySelector('body').appendChild(document.createElement('h1')).innerText= "POST Successful: " + object.id)
+            .catch(error => document.querySelector('body').appendChild(document.createElement('h1')).innerText= "Unauthorized Access" + JSON.stringify(error.message));
+  }
